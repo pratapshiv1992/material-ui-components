0161a29dc74be9bea144e909de06269aaef6a11d
shiv pratap singh <pratapshiv1992@gmail.com>
2020-10-22 00:28:32 +0530

    cleanup

--- src/components/Error.jsx
+++
@@ -1,4 +0,0 @@
-import React, {useEffect, useState} from 'react';
-import Button from "@material-ui/core/Tex";
-import {makeStyles} from "@material-ui/core/styles";
-import CircularProgress from '@material-ui/core/CircularProgress';
